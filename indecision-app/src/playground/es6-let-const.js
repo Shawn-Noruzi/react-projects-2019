070f815974e31644a97365b5aea78ed058b76bb4
shawnSFU <snoruzi@sfu.ca>
2019-02-27 16:12:36 -0800

    ES6 Arrow function practices using map( ), objects, and simple math.

---
+++ indecision-app/src/playground/es6-let-const.js
@@ -0,0 +1,39 @@
+/*
+ * 
+ * es6 arrow functions - always anonymous.
+ * const name = (param x) => return x*x
+ * 
+ * 1.there's an implicit return 
+ * 2.arguments object no longer bound
+ * 3. 'this' is no longer bound
+ * 
+ * es5 vs es6
+ * functions vs arrow functions
+ * 
+ * comes down to where you're able to use 'this' at. 
+ * 
+ * es6 removes need for const that = this;
+ * 
+ * inside objects just use functionName() {...arrow function ...stuff}
+ * 
+ */
+
+const user = {
+    name: 'Andrew',
+    cities: ['London', 'Japan', 'Hong Kong'],
+    placesLived() {
+        return this.cities.map((cities) => this.name + ' has lived in ' + cities)
+    }
+};
+
+console.log(user.placesLived());
+
+
+const multiplier = {
+    numbers: [3, 2, 1],
+    multiplyBy: 2,
+    multiply() {
+        return this.numbers.map((number) => number * this.multiplyBy)
+    }
+};
+console.log(multiplier.multiply());
