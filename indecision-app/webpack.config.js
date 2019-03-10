1f61093f93b3089617926b8a4cc955c74f92d0ca
shawnSFU <snoruzi@sfu.ca>
2019-03-09 16:24:10 -0800

    Webpack: DevTool -> Webpack-dev-server installed and swapped in place of live-server.
    Updated webpack to included dev-server, removed Babel script.
    
    Webpack.config.js -> devServer -> contentBase  -- chooses the location to serve static files from.

--- indecision-app/webpack.config.js
+++ indecision-app/webpack.config.js
@@ -16,7 +16,10 @@ module.exports = {
             exclude: /node_modules/
         }]
     },
-    devtool: 'cheap-module-eval-source-map'
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
 };
 
 //loader - customize behavior of webpack when you load a given file (.js, .jpeg, .scss)
