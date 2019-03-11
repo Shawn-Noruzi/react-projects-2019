d5e29b8f8ad3174a7806c3292aa9d389c4e87b25
shawnSFU <snoruzi@sfu.ca>
2019-03-11 15:53:35 -0700

    Added a Modal for when a user clicks "What should I do" that displays the dynamic array data onClick.
    IndecisionApp has new handleClearSelectOption function -> passed as a prop to OptionModal component.

--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -3,11 +3,20 @@ import AddOption from './AddOption';
 import Header from './Header';
 import Action from './Action';
 import Options from './Options';
+import OptionModal from './OptionModal';
 
 export default class IndecisionApp extends React.Component {
     state = {
-    options: []
+        options: [],
+        selectedOption: undefined
     };
+
+    handleClearSelectedOption = () => {
+        this.setState(() => ({ selectedOption: undefined }));
+    } 
+
+    
+
     handleDeleteOptions = () => {
         this.setState(() => ({ options: [] }));
     };
@@ -21,7 +30,9 @@ export default class IndecisionApp extends React.Component {
     handlePick = () => {
         const randomNum = Math.floor(Math.random() * this.state.options.length);
         const option = this.state.options[randomNum];
-        alert(option);
+        this.setState(() => ({
+            selectedOption: option
+        }));
     };
 
     handleAddOption = (option) => {
@@ -76,6 +87,10 @@ export default class IndecisionApp extends React.Component {
                     handleDeleteOption={this.handleDeleteOption}
                 />
                 <AddOption handleAddOption={this.handleAddOption} />
+                <OptionModal
+                    selectedOption={this.state.selectedOption}
+                    handleClearSelectedOption={this.handleClearSelectedOption}
+                />
             </div>
         );
     }
