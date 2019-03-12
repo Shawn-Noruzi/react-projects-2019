f969e3f7245fb3de45a343b85d836a981abbcca1
shawnSFU <snoruzi@sfu.ca>
2019-03-11 23:55:56 -0700

    Created widget__message, widget-header, widget-header__title styles.
    Display: flex -> justify-content: space-between  to align 'Your options' and 'Remove all options' side by side.

--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -1,101 +1,99 @@
-import React from 'react';
-import AddOption from './AddOption';
-import Header from './Header';
-import Action from './Action';
-import Options from './Options';
-import OptionModal from './OptionModal';
+import React from "react";
+import AddOption from "./AddOption";
+import Header from "./Header";
+import Action from "./Action";
+import Options from "./Options";
+import OptionModal from "./OptionModal";
 
 export default class IndecisionApp extends React.Component {
-    state = {
-        options: [],
-        selectedOption: undefined
-    };
+  state = {
+    options: [],
+    selectedOption: undefined
+  };
 
-    handleClearSelectedOption = () => {
-        this.setState(() => ({ selectedOption: undefined }));
-    } 
+  handleClearSelectedOption = () => {
+    this.setState(() => ({ selectedOption: undefined }));
+  };
 
-    
+  handleDeleteOptions = () => {
+    this.setState(() => ({ options: [] }));
+  };
 
-    handleDeleteOptions = () => {
-        this.setState(() => ({ options: [] }));
-    };
+  handleDeleteOption = optionToRemove => {
+    this.setState(prevState => ({
+      options: prevState.options.filter(option => optionToRemove !== option)
+    }));
+  };
 
-    handleDeleteOption = (optionToRemove) => {
-        this.setState(prevState => ({
-            options: prevState.options.filter(option => optionToRemove !== option)
-        }));
-    };
+  handlePick = () => {
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomNum];
+    this.setState(() => ({
+      selectedOption: option
+    }));
+  };
 
-    handlePick = () => {
-        const randomNum = Math.floor(Math.random() * this.state.options.length);
-        const option = this.state.options[randomNum];
-        this.setState(() => ({
-            selectedOption: option
-        }));
-    };
-
-    handleAddOption = (option) => {
-        if (!option) {
-            return "Enter valid value to add item";
-        } else if (this.state.options.indexOf(option) > -1) {
-            return "this option already exists";
-        }
-        this.setState(prevState => ({ options: prevState.options.concat(option) }));
-    };
-    //lifecycle methods
-    componentDidMount () {
-        //try catch for json syntax errors
-        try {
-            const json = localStorage.getItem("options");
-            const options = JSON.parse(json);
-
-            if (options) {
-                this.setState(() => ({ options }));
-            }
-        } catch (e) {
-            //Do nothing if json invalid
-        }
+  handleAddOption = option => {
+    if (!option) {
+      return "Enter valid value to add item";
+    } else if (this.state.options.indexOf(option) > -1) {
+      return "this option already exists";
     }
-    componentDidUpdate (prevProps, prevState) {
-        if (prevState.options.length !== this.state.options.length) {
-            //takes options object and makes it into a string.
-            const json = JSON.stringify(this.state.options);
+    this.setState(prevState => ({ options: prevState.options.concat(option) }));
+  };
+  //lifecycle methods
+  componentDidMount() {
+    //try catch for json syntax errors
+    try {
+      const json = localStorage.getItem("options");
+      const options = JSON.parse(json);
 
-            //store the json stringed object, with key of options, into localStorage.
-            localStorage.setItem("options", json);
-            console.log("saving data ");
-        }
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (e) {
+      //Do nothing if json invalid
     }
-    componentWillUnmount () {
-        console.log("component will unmount");
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      //takes options object and makes it into a string.
+      const json = JSON.stringify(this.state.options);
+
+      //store the json stringed object, with key of options, into localStorage.
+      localStorage.setItem("options", json);
+      console.log("saving data ");
     }
-  
+  }
+  componentWillUnmount() {
+    console.log("component will unmount");
+  }
 
-    render() {
-        const subtitle = "Put your life in the hands of an apple";
-        return (
-            <div>
-                <Header subtitle={subtitle} />
-                <div className="container">
-                    <Action
-                        hasOptions={this.state.options.length > 0}
-                        handlePick={this.handlePick}
-                    />
-                    <Options
-                        options={this.state.options}
-                        handleDeleteOptions={this.handleDeleteOptions}
-                        handleDeleteOption={this.handleDeleteOption}
-                    />
-                    <AddOption handleAddOption={this.handleAddOption} />
-                    <OptionModal
-                        selectedOption={this.state.selectedOption}
-                        handleClearSelectedOption={this.handleClearSelectedOption}
-                    />
-                </div>
-
-            </div>
-        );
-    }
-};
-
+  render() {
+    const subtitle = "Put your life in the hands of an apple";
+    return (
+      <div>
+        <Header subtitle={subtitle} />
+        <div className="container">
+          <div className="widget">
+            {" "}
+            <Action
+              hasOptions={this.state.options.length > 0}
+              handlePick={this.handlePick}
+            />
+            <Options
+              options={this.state.options}
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption}
+            />
+            <AddOption handleAddOption={this.handleAddOption} />
+            <OptionModal
+              selectedOption={this.state.selectedOption}
+              handleClearSelectedOption={this.handleClearSelectedOption}
+            />{" "}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -3,13 +3,16 @@ import Option from './Option';
 
 const Options = (props) => (
     <div>
+        <div className="widget-header">
+            <h3 className="widget-header__title">Your Options</h3>
         <button
             className="button button--link"
             onClick={props.handleDeleteOptions}>
             Remove All Options
         </button>
+        </div>
         {props.options.length === 0 && (
-            <p>Add some options into the options array</p>
+            <p className="widget__message">Add some options into the options array</p>
         )}
         {props.options.map(option => (
             <Option
