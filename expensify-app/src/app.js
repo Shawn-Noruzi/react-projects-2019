9856866ffb9a9b54ef08ed3b4c8589bfb799799c
shawnSFU <snoruzi@sfu.ca>
2019-03-18 23:26:26 -0700

    Introduction to react-routing.
    Imported BrowserRouter, Route from react-router-dom. Created 4 unique pages. exact={true} prop used for specific page pathing.

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,6 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Expensify</p>, document.getElementById("app"));
+const ExpenseDashboardPage = () => (
+    <div>
+        this is from my d-board component
+        </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        this is from my Add expense component
+        </div>
+);
+
+//edit
+const EditExpensePage = () => (
+    <div>
+        this is the edit page
+    </div>
+)
+//help
+const HelpPage = () => (
+    <div>
+    help page
+    </div>
+)
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+        </div>
+
+
+    </BrowserRouter>
+    );
+
+ReactDOM.render(routes, document.getElementById("app"));
