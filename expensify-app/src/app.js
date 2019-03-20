70a95b944762dc98bf9bf5a787dc0ac5c744d228
shawnSFU <snoruzi@sfu.ca>
2019-03-20 14:11:33 -0700

    Add Link function from React-router and created 5 new pages + header.

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -32,19 +32,32 @@ const HelpPage = () => (
 
 const NotFoundPage = () => (
     <div>
-    404 -  Not Found!
+    404 -  <Link to="/">Go Home</Link>
     </div>
+)
+
+const Header = () => (
+<Header>
+  <h1>Expensify</h1>  
+  <Link to="/">Home</Link>
+  <Link to="/create">Create</Link>
+  <Link to="/edit">Edit</Link>
+  <Link to="/help">Help</Link>
+</Header>
 )
 
 const routes = (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" component={ExpenseDashboardPage} exact={true} />
-            <Route path="/create" component={AddExpensePage} />
-            <Route path="/edit" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage} />
-            <Route component={NotFoundPage}/>
-        </Switch>
+        <div>
+            <Header/>
+            <Switch>
+                <Route path="/" component={ExpenseDashboardPage} exact={true} />
+                <Route path="/create" component={AddExpensePage} />
+                <Route path="/edit" component={EditExpensePage} />
+                <Route path="/help" component={HelpPage} />
+                <Route component={NotFoundPage}/>
+            </Switch>
+        </div>
     </BrowserRouter>
     );
 
