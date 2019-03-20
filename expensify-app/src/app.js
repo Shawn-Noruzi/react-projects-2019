969c475dc2ab3dd2757ceb421fc278b8bc3c4eca
shawnSFU <snoruzi@sfu.ca>
2019-03-19 22:11:07 -0700

    Switch used for 404 page. Runs through routes until it finds the one it needs.

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -20,6 +20,7 @@ const AddExpensePage = () => (
 const EditExpensePage = () => (
     <div>
         this is the edit page
+
     </div>
 )
 //help
@@ -28,16 +29,22 @@ const HelpPage = () => (
     help page
     </div>
 )
+
+const NotFoundPage = () => (
+    <div>
+    404 -  Not Found!
+    </div>
+)
+
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path="/" component={ExpenseDashboardPage} exact={true} />
             <Route path="/create" component={AddExpensePage} />
             <Route path="/edit" component={EditExpensePage} />
             <Route path="/help" component={HelpPage} />
-        </div>
-
-
+            <Route component={NotFoundPage}/>
+        </Switch>
     </BrowserRouter>
     );
 
