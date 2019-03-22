2ab9de4f093900da87fc46afc837d84000fd4c45
shawnSFU <shawn.noruzi@gmail.com>
2019-03-21 19:59:57 -0700

    Had to rebuild app due to hardware changes - used boiler.
    Added Link, a header with links to each various page.

---
+++ expensify/src/app.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard component
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    This is from my add expense component
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This is from my edit expense component
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    This is from my help component
+  </div>
+);
+
+const NotFoundPage = () => (
+  <div>
+    404! <Link to="/">Go Home</Link>
+  </div>
+);
+
+const Header = () => (
+ <header>
+   <h1>Expensify</h1>
+   <Link to="/">Go Home </Link>
+   <Link to="/create">Create </Link>
+   <Link to="/edit">Edit </Link>
+   <Link to="/help">Help </Link>
+
+ </header>
+);
+const routes = (
+  <BrowserRouter>
+  <div>
+  <Header />
+  <Switch>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+      <Route component={NotFoundPage} />
+  </Switch>
+  </div>
+
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
