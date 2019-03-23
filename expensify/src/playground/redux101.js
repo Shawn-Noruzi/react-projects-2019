fddced38c7cd491b35ab63ed57fa17bbf3747a90
shawnSFU <shawn.noruzi@gmail.com>
2019-03-22 21:00:39 -0700

    Refactoring + Organizing code: Action Generators will be used to give errors for typos.

--- expensify/src/playground/redux101.js
+++ expensify/src/playground/redux101.js
@@ -1,18 +1,38 @@
 import { createStore } from "redux";
 
+//Action generators - functions that return action objects
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy
+});
+
+const decrementCount = ({decrementBy = -1} = {}) => ({
+type: 'DECREMENT',
+decrementBy
+});
+
+const setCount = ({count}) => ({
+  type: 'SET',
+  count
+  });
+
+  
+const resetCount = () => ({
+  type: 'RESET',
+  count: 0
+  });
+
+
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
-      const incrementBy =
-        typeof action.incrementBy === "number" ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
     case "DECREMENT":
-      const decrementBy =
-        typeof action.decrementBy === "number" ? action.decrementBy : 1;
       return {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
       };
     case "SET":
       return {
@@ -31,21 +51,12 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({
-  type: "INCREMENT",
-  incrementBy: 5
-});
 
+store.dispatch(incrementCount({incrementBy: 5}));
+store.dispatch(incrementCount());
+store.dispatch(decrementCount({decrementBy: 99}));
 store.dispatch({
   type: "RESET"
 });
-
-store.dispatch({
-  type: "DECREMENT"
-});
-
-store.dispatch({
-  type: "SET",
-  count: 101
-});
-console.log(store.getState());
+store.dispatch(setCount({count: 11991199}));
+store.dispatch(resetCount());
