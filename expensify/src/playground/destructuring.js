0232a6e2abe8c94a903d42d99fd30e35aa5bd35c
shawnSFU <shawn.noruzi@gmail.com>
2019-03-22 20:41:40 -0700

    Object and Array destructuring (focuses on positions when defining) introduced.

--- expensify/src/playground/destructuring.js
+++ expensify/src/playground/destructuring.js
@@ -1,31 +1,41 @@
-console.log('deconstructuring')
+// //object destructuring 
 
-const person = {
-    name:'andrew',
-    age:26,
+// console.log('deconstructuring')
 
-    location: {
-        city: 'phili',
-        temp: 92
-    }
-};
-const {name = 'anon',age} = person;
-//const name = person.name;
+// const person = {
+//     name:'andrew',
+//     age:26,
 
-console.log(`${name} is ${age}`);
-//renaming , default values
-const {city,temp: temperature} = person.location;
-console.log(`its ${temperature} in ${city}`);
+//     location: {
+//         city: 'phili',
+//         temp: 92
+//     }
+// };
+// const {name = 'anon',age} = person;
+// //const name = person.name;
 
-const book = {
-    title: 'ego',
-    author:'ryan',
-    publisher: {
+// console.log(`${name} is ${age}`);
+// //renaming , default values
+// const {city,temp: temperature} = person.location;
+// console.log(`its ${temperature} in ${city}`);
+
+// const book = {
+//     title: 'ego',
+//     author:'ryan',
+//     publisher: {
         
-    }
-}
+//     }
+// }
 
-const {title, author = 'anon'} = book;
-const {name: renamed = 'slefpublished'} = book.publisher;
+// const {title, author = 'anon'} = book;
+// const {name: renamed = 'slefpublished'} = book.publisher;
 
-console.log(`its ${renamed} in ${title}`);
+// console.log(`its ${renamed} in ${title}`);
+
+// //array destructing
+
+const address = ['1299 s juniper st', 'phili', 'pensylvania', '19191']
+
+const [,,state, zip] = address;
+
+console.log(`You're in ${zip }in the state of ${state}`);
