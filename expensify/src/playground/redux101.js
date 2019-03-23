486f7bf823c6b8f1cf6e33e67ea407ade8be3bf3
shawnSFU <shawn.noruzi@gmail.com>
2019-03-22 18:42:53 -0700

    Actions - Cases for increment, decrement, reset count state. action is 2nd argument for createStore() store.dispatch(){type: "CASENAME"}
    store.getState() retrieves object in redux store.

--- expensify/src/playground/redux101.js
+++ expensify/src/playground/redux101.js
@@ -1,7 +1,39 @@
-import {createStore} from 'redux';
- 
-const store = createStore((state = {count:0}) => {
-return state;
+import { createStore } from "redux";
+
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      };
+      case "DECREMENT":
+      return {
+        count: state.count - 1
+      };
+      case "RESET":
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
 });
 
+console.log(store.getState());
+
+//Actions - object sent to store
+
+//increment count
+store.dispatch({
+  type: "INCREMENT"
+});
+
+  store.dispatch({
+    type: "RESET"
+  });
+
+  store.dispatch({
+    type: "DECREMENT"
+  });
+  //reset 
 console.log(store.getState());
