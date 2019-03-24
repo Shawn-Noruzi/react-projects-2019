7fd392e90d4e5acfb41ed097f2d88673b6ca40b9
shawnSFU <shawn.noruzi@gmail.com>
2019-03-24 15:51:43 -0700

    Filtering for date, amount and text created for objects in the redux store.

--- expensify/src/playground/redux-expensify.js
+++ expensify/src/playground/redux-expensify.js
@@ -133,6 +133,22 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
   }
 };
 
+
+//timestamps (any + or - number)
+//January 1st 1970 (unix epoch)
+
+//Get visible expenses
+const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) => {
+
+  return expenses.filter((expense) => {
+    const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+    const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+    return startDateMatch && endDateMatch && textMatch;
+  });
+};
+
 //STORE CREATION
 
 const store = createStore(
@@ -143,24 +159,26 @@ const store = createStore(
 );
 
 store.subscribe(() => {
-  console.log(store.getState());
+  const state = store.getState();
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+  console.log(visibleExpenses);
 });
 
-// const expenseOne = store.dispatch(
-//   addExpense({ description: "Rent", amount: 100 })
-// );
-// const expenseTwo = store.dispatch(
-//   addExpense({ description: "House", amount: 300 })
-// );
+const expenseOne = store.dispatch(
+  addExpense({ description: "Rent", amount: 100, createdAt: 1000 })
+);
+const expenseTwo = store.dispatch(
+  addExpense({ description: "House", amount: 300, createdAt: -1000 })
+);
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
-// store.dispatch(setTextFilter("rent"));
+store.dispatch(setTextFilter("house"));
 // store.dispatch(setTextFilter());
 // store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
-store.dispatch(setStartDate(125));
-store.dispatch(setEndDate(335));
+//store.dispatch(setStartDate(125));
+// store.dispatch(setEndDate(335));
 
 
 
