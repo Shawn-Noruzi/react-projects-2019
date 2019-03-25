1e4315f857615c582f106a9386daf1ea3a5363c2
shawnSFU <shawn.noruzi@gmail.com>
2019-03-24 18:03:16 -0700

    Organized redux and incorporated it into the expensify-app.

--- expensify/src/app.js
+++ expensify/src/app.js
@@ -1,7 +1,47 @@
-import React from 'react';
-import AppRouter from './routers/AppRouter';
-import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import React from "react";
+import AppRouter from "./routers/AppRouter";
+import ReactDOM from "react-dom";
+import configureStore from "./store/configureStore";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
+
+const store = configureStore();
+
+store.subscribe(() => {
+console.log(store.getState());
+  });
+  
+//add expense -> water bill
+console.log("add water bill");
+store.dispatch(
+  addExpense({ description: "water bill"})
+);
+
+//add expense -> gas bill
+console.log("add gass bill");
+store.dispatch(
+  addExpense({ description: "gas bill"})
+);
+
+//setTextFilter -> bill  (2i tems ) -> water (1 item)
+
+console.log("text filter for bill");
+store.dispatch(setTextFilter("bill"));
+
+console.log("text filter for water");
+store.dispatch(setTextFilter("water"));
+
+//getVisibleExpenses -> print visibles ones to screen
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+  
+
+console.log(store.getState());
+
+ReactDOM.render(<AppRouter />, document.getElementById("app"));
