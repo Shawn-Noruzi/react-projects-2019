3f99b9e62c9a418c3d85eadb77e212654619467a
shawnSFU <shawn.noruzi@gmail.com>
2019-03-27 18:05:49 -0700

    Added react-dates (calender picker tool) and moment.js  (used to Parse, validate, manipulate, and display dates and times in JavaScript)
    moment() used to grab current time.
    
    Added # of months to be set to 1 in the calender tool. (for clarity)
    
    Added isOutsideRange to allow older dates to be selectable.

--- expensify/src/components/ExpenseForm.js
+++ expensify/src/components/ExpenseForm.js
@@ -1,10 +1,18 @@
 import React from "react";
+import moment from "moment";
+import { SingleDatePicker } from "react-dates";
+import "react-dates/lib/css/_datepicker.css";
+import 'react-dates/initialize';
 
+const now = moment();
+console.log(now.format("MMM Do, YYYY"));
 export default class ExpenseForm extends React.Component {
   state = {
     description: "",
     note: "",
-    amount: ""
+    amount: "",
+    createdAt: moment(),
+    calenderFocused: false
   };
   onDescriptionChange = e => {
     const description = e.target.value;
@@ -23,6 +31,13 @@ export default class ExpenseForm extends React.Component {
     }
   };
 
+  onDateChange = createdAt => {
+    this.setState(() => ({ createdAt }));
+  };
+
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calenderFocused: focused }));
+  };
   render() {
     return (
       <div>
@@ -41,7 +56,14 @@ export default class ExpenseForm extends React.Component {
             value={this.state.amount}
             onChange={this.onAmountChange}
           />
-
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calenderFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
+          />
           <textarea
             placeholder="add a note for your expense (optional)"
             note={this.state.note}
