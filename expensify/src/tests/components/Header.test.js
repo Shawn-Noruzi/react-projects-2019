e21561aedd328f4bad8a137485ab1a988d98085f
shawnSFU <shawn.noruzi@gmail.com>
2019-04-01 13:14:59 -0700

    Installation of testing utilities
    -Enzyme + Enzyme-adapter-react16 + raf
    -setupTest.js : configures the environment for the testing utilities.
    
    Testing of Header
    -find method searches through the wrapper's tree nodes until it finds the selector provided
    -text method asserts the content within the component's selector.

--- expensify/src/tests/components/Header.test.js
+++ expensify/src/tests/components/Header.test.js
@@ -1,11 +1,15 @@
 //react-test-renderer
-
-import ReactShallowRenderer from 'react-test-renderer/shallow';
 import React from 'react';
+import {shallow} from 'enzyme';
+
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
- const renderer = new ReactShallowRenderer();
- renderer.render(<Header />);
- expect(renderer.getRenderOutput()).toMatchSnapshot();
+
+const wrapper = shallow(<Header />);
+expect(wrapper.find('h1').text()).toBe('Expensify');
+
+//  const renderer = new ReactShallowRenderer();
+//  renderer.render(<Header />);
+//  expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
