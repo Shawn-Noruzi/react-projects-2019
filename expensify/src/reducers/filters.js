781838afde762b5fd343fcd20f2fe04578b4df72
shawnSFU <shawn.noruzi@gmail.com>
2019-03-31 17:30:30 -0700

    Tests for reducer/filter.js added.
    Had to define state + action then feed into the default exported filtersReducer function created from before.

--- expensify/src/reducers/filters.js
+++ expensify/src/reducers/filters.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 
 const filtersReducerDefaultState = {
     text: "",
-    sortBy: 'amount',
+    sortBy: 'date',
     startDate: moment().startOf('month'),
     endDate: moment().endOf('month')
   };
--- expensify/src/tests/selectors/expenses.test.js
+++ expensify/src/tests/selectors/expenses.test.js
@@ -1,33 +1,26 @@
-import selectExpenses from "../../selectors/expenses";
 import moment from "moment";
+import selectExpenses from "../../selectors/expenses";
 
-const expenses = [
-  {
-    id: "1",
-    description: "gum",
-    note: "",
+
+const expenses = [{
+    id: '1',
+    description: 'Gum',
+    note: '',
     amount: 195,
     createdAt: 0
-  },
-  {
-    id: "2",
-    description: "rent",
-    note: "",
-    amount: 100095,
-    createdAt: moment(0)
-      .subtract(4, "days")
-      .valueOf()
-  },
-  {
-    id: "3",
-    description: "Credit Card",
-    note: "",
-    amount: 4595,
-    createdAt: moment(0)
-      .add(4, "days")
-      .valueOf()
-  }
-];
+  }, {
+    id: '2',
+    description: 'Rent',
+    note: '',
+    amount: 109500,
+    createdAt: moment(0).subtract(4, 'days').valueOf()
+  }, {
+    id: '3',
+    description: 'Credit Card',
+    note: '',
+    amount: 4500,
+    createdAt: moment(0).add(4, 'days').valueOf()
+  }];
 
 test("should filter by text value", () => {
   const filters = {
@@ -37,17 +30,50 @@ test("should filter by text value", () => {
     endDate: undefined
   };
   const result = selectExpenses(expenses, filters);
-  expect(result).toEqual([expenses[2], expenses[0]]);
+  expect(result).toEqual([expenses[2], expenses[1]]);
 });
 
 
 test("should filter by startDate", () => {
   const filters = {
     text: '',
-    sortBy: "date",
+    sortBy: 'date',
     startDate: moment(0),
     endDate: undefined
   };
   const result = selectExpenses(expenses, filters);
   expect(result).toEqual([expenses[2], expenses[0]]);
+});
+
+test('filter by end date', () => {
+    const filters = {
+        text: '',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: moment(0).add(2,'days')
+      };
+      const result = selectExpenses(expenses, filters);
+      expect(result).toEqual([expenses[0], expenses[1]]);
+});
+
+test('sort  by date', () => {
+    const filters = {
+        text: '',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: undefined
+      };
+      const result = selectExpenses(expenses, filters);
+      expect(result).toEqual([expenses[2], expenses[0], expenses[1]]);
+});
+
+test('sort  by amount', () => {
+    const filters = {
+        text: '',
+        sortBy: 'amount',
+        startDate: undefined,
+        endDate: undefined
+      };
+      const result = selectExpenses(expenses, filters);
+      expect(result).toEqual([expenses[1], expenses[2], expenses[0]]);
 });
