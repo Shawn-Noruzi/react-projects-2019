e515bd7bc47bc162be050976669449cb4f2a7427
shawnSFU <shawn.noruzi@gmail.com>
2019-04-02 14:54:06 -0700

    Event handler testing
    Event handlers expect events to be passed in. Jest allows us to simulate actions (and other intended actions like prevent default) to do this.
    
    Before + After snapshots were taken to display the addition of an error throw within the ExpenseForm submission.

--- expensify/src/tests/components/ExpenseForm.test.js
+++ expensify/src/tests/components/ExpenseForm.test.js
@@ -15,3 +15,14 @@ test('should render expenseform with expense data ', () => {
     const wrapper = shallow(<ExpenseForm  expense = {expenses[1]} />);
     expect(wrapper).toMatchSnapshot();
 });
+
+test('should render error for invalid form submission', () => {
+    const wrapper = shallow(<ExpenseForm  />);
+    expect(wrapper).toMatchSnapshot();
+    //prevents undefined behavior being passed into event handler
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => {}
+    });
+    expect(wrapper.state('error').length).toBeGreaterThan(0)
+    expect(wrapper).toMatchSnapshot();
+});
