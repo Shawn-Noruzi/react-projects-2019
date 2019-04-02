a198d8f0a69c72fdec0fece17216ab42b9ffd0bf
shawnSFU <shawn.noruzi@gmail.com>
2019-04-02 14:34:56 -0700

    Mocking moment for testing purposes.
    Moment() was the reason for snapshots having different timestamps as it grabbed a new time every run.
    
    Issue: the time still changes even though we've mocked the moment() function. Will consult documentation some more.

--- expensify/src/app.js
+++ expensify/src/app.js
@@ -5,7 +5,8 @@ import { Provider } from "react-redux";
 import configureStore from "./store/configureStore";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
-
+import "react-dates/lib/css/_datepicker.css";
+import "react-dates/initialize";
 import { addExpense } from "./actions/expenses";
 import { setTextFilter } from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
---
+++ expensify/src/tests/components/ExpenseForm.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import ExpenseForm  from '../../components/ExpenseForm';
+import expenses from '../fixtures/expenses';
+
+
+test('should render expenseform with no data ', () => {
+    const wrapper = shallow(<ExpenseForm />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+
+
+test('should render expenseform with expense data ', () => {
+    const wrapper = shallow(<ExpenseForm  expenses = {expenses[1]} />);
+    expect(wrapper).toMatchSnapshot();
+});
