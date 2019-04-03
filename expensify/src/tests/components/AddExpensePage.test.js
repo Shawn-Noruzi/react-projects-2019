1ca8dab006eb893f5bb558d82a4a52af63585cef
shawnSFU <shawn.noruzi@gmail.com>
2019-04-02 23:42:18 -0700

    Tests for EditExpense
    Testing for an onSubmit button that passed props into a function.
    
    Also tested a remove button that removed an expense based on its passed id value.
    
    beforeEach greatly simplifies the testing of multiple functions.

--- expensify/src/tests/components/AddExpensePage.test.js
+++ expensify/src/tests/components/AddExpensePage.test.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import {AddExpensePage} from '../../components/AddExpense';
-import expenses from '../fixtures/expenses';
+import React from "react";
+import { shallow } from "enzyme";
+import { AddExpensePage } from "../../components/AddExpense";
+import expenses from "../fixtures/expenses";
 
-let onSubmit, history, wrapper;
+let addExpense, history, wrapper;
 beforeEach(() => {
-  onSubmit = jest.fn();
-    history = { push: jest.fn()};
-     wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+  addExpense = jest.fn();
+  history = { push: jest.fn() };
+  wrapper = shallow(
+    <AddExpensePage addExpense={addExpense} history={history} />
+  );
 });
 
-test('should add expense correctly', () => {
-
- expect(wrapper).toMatchSnapshot();
+test("should add expense correctly", () => {
+  expect(wrapper).toMatchSnapshot();
 });
 
-test(' should handle onSubmit ', () => {
-
-    wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
-    expect(history.push).toHaveBeenLastCalledWith('/');
-    expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
+test(" should handle onSubmit ", () => {
+  wrapper.find("ExpenseForm").prop("onSubmit")(expenses[1]);
+  expect(history.push).toHaveBeenLastCalledWith("/");
+  expect(addExpense).toHaveBeenLastCalledWith(expenses[1]);
 });
