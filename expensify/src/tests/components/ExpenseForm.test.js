4244afb7710355cd816395ec5a21422043571bbe
shawnSFU <shawn.noruzi@gmail.com>
2019-04-02 19:29:55 -0700

    Tests for input functions used in ExpenseForm component
    -Description input
    -Invalid form submit
    -Note input
    -Amount valid input / invalid input
    
    Using .find we accessed the part of the component we want and since we have multiple input fields the .at() function allows us to access the indexes of these input fields.

--- expensify/src/tests/components/ExpenseForm.test.js
+++ expensify/src/tests/components/ExpenseForm.test.js
@@ -26,3 +26,40 @@ test('should render error for invalid form submission', () => {
     expect(wrapper.state('error').length).toBeGreaterThan(0)
     expect(wrapper).toMatchSnapshot();
 });
+
+test('should set input on description change ', () => {
+    const value = 'New Description';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(0).simulate('change', {
+        target:{value}
+    });
+    expect(wrapper.state('description')).toBe(value);
+});
+
+
+test('should set note on text area change ', () => {
+    const value = 'New Note';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('textarea').at(0).simulate('change', {
+        target:{value}
+    });
+    expect(wrapper.state('note')).toBe(value);
+});
+
+test('should set amount if valid input ', () => {
+    const value = '23.50';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(1).simulate('change', {
+        target:{value}
+    });
+    expect(wrapper.state('amount')).toBe(value);
+});
+
+test('should not set amount if invalid input ', () => {
+    const value = '12.122';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(1).simulate('change', {
+        target:{value}
+    });
+    expect(wrapper.state('amount')).toBe('');
+});
