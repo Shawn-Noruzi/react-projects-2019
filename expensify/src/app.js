aa55299d963c733c6ea1491f024b0a53db183ce3
shawnSFU <shawn.noruzi@gmail.com>
2019-04-07 17:38:15 -0700

    Optimization of Bundle.js
    Creating seperate CSS files using extract-text-webpack-plugin to minify further bundle.js.

--- expensify/src/app.js
+++ expensify/src/app.js
@@ -7,9 +7,6 @@ import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import "react-dates/lib/css/_datepicker.css";
 import "react-dates/initialize";
-import { addExpense } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
-import getVisibleExpenses from "./selectors/expenses";
 
 const store = configureStore();
 
